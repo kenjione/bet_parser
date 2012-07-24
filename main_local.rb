1d3915864260f4845a0aad21bee2808dac3e76e3
Alex Chernyshev <alex.kenjione@gmail.com>
2012-07-25 01:48:27 +0400

    запилено создание штрих-кода

--- main_local.rb
+++ main_local.rb
@@ -5,6 +5,8 @@ require "hpricot"
 require "open-uri"
 require "mechanize"
 require 'time'
+require 'rghost'
+require 'rghost_barcode'
 
 require 'rubygems'
 require 'sqlite3'
@@ -102,4 +104,9 @@ begin
 end
 
 
+# TODO: запилить в отдельный рбшник чтобы не пересоздавал баркод при проверке
 
+RGhost::Config::GS[:path]= "C:\\gs\\gs8.60\\bin\\gswin32c.exe"
+doc=RGhost::Document.new
+doc.barcode_code128("#{bets[-1].id}",{:text=>{:size=>8}, :parsefnc=>true, :enable=>[:text]})
+doc.render :jpg, :resolution => 500, :filename => "my_barcode.jpg"
